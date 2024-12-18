1e8e4c12b4fd5f274a108db38cea662ce4e5d7c4
Oleksandr Dushnyi <sdusnij@gmail.com>
2024-12-18 16:45:30 +0200

    fix: updated news module

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Query,
   UseGuards,
+  Req,
 } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { CreateNewsDto } from './dto/create-news.dto';
@@ -20,8 +21,9 @@ export class NewsController {
 
   @UseGuards(AuthUserGuard)
   @Post()
-  create(@Body() createNewsDto: CreateNewsDto) {
-    return this.newsService.create(createNewsDto);
+  create(@Body() createNewsDto: CreateNewsDto, @Req() req) {
+    const token = req.user;
+    return this.newsService.create(createNewsDto, token);
   }
 
   @UseGuards(AuthUserGuard)
@@ -33,25 +35,34 @@ export class NewsController {
       sort?: string;
       companyId?: string;
     },
+    @Req() req,
   ) {
-    return this.newsService.findAll(query);
+    const token = req.user;
+    return this.newsService.findAll(query, token);
   }
 
   @UseGuards(AuthUserGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.newsService.findOne(+id);
+  findOne(@Param('id') id: string, @Req() req) {
+    const token = req.user;
+    return this.newsService.findOne(+id, token);
   }
 
   @UseGuards(AuthUserGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNewsDto: UpdateNewsDto) {
-    return this.newsService.update(+id, updateNewsDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateNewsDto: UpdateNewsDto,
+    @Req() req,
+  ) {
+    const token = req.user;
+    return this.newsService.update(+id, updateNewsDto, token);
   }
 
   @UseGuards(AuthUserGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.newsService.remove(+id);
+  remove(@Param('id') id: string, @Req() req) {
+    const token = req.user;
+    return this.newsService.remove(+id, token);
   }
 }
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateNewsDto } from './dto/create-news.dto';
 import { UpdateNewsDto } from './dto/update-news.dto';
 import { PrismaService } from 'prisma/prisma.service';
@@ -7,7 +11,7 @@ import { PrismaService } from 'prisma/prisma.service';
 export class NewsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(createNewsDto: CreateNewsDto) {
+  async create(createNewsDto: CreateNewsDto, token) {
     const { title, content, companyId } = createNewsDto;
 
     const companyExists = await this.prisma.company.findUnique({
@@ -20,49 +24,95 @@ export class NewsService {
       );
     }
 
-    return this.prisma.news.create({ data: { title, content, companyId } });
+    if (token.roleId === 1) {
+      return this.prisma.news.create({ data: { title, content, companyId } });
+    } else if (token.roleId === 2 || token.roleId === 3) {
+      if (!token.companyId) {
+        throw new ForbiddenException('You are not assigned to any company');
+      }
+
+      if (token.companyId !== companyId) {
+        throw new ForbiddenException(
+          'Access denied to create news for this company',
+        );
+      }
+
+      return this.prisma.news.create({ data: { title, content, companyId } });
+    } else {
+      throw new ForbiddenException('Access denied');
+    }
   }
 
-  async findAll(query: { page?: number; sort?: string; companyId?: string }) {
+  async findAll(
+    query: { page?: number; sort?: string; companyId?: string },
+    token: string,
+  ) {
     const { page = 1, sort = 'createdAt', companyId } = query;
 
-    return this.prisma.news.findMany({
-      where: {
-        companyId: companyId ? parseInt(companyId) : undefined,
-      },
+    let whereClause: any = {};
+
+    if (token['roleId'] === 1) {
+      whereClause.companyId = companyId ? parseInt(companyId) : undefined;
+    } else if (token['roleId'] === 2 || token['roleId'] === 3) {
+      if (!token['companyId']) {
+        throw new ForbiddenException('You are not assigned to any company');
+      }
+
+      if (companyId && parseInt(companyId) !== token['companyId']) {
+        throw new ForbiddenException('Access denied to this company news');
+      }
+
+      whereClause.companyId = token['companyId'];
+    } else {
+      throw new ForbiddenException('Access denied');
+    }
+
+    const news = await this.prisma.news.findMany({
+      where: whereClause,
       orderBy: { [sort]: 'asc' },
       skip: (page - 1) * 10,
       take: 10,
     });
-  }
 
-  async findOne(id: number) {
-    const news = await this.prisma.news.findUnique({ where: { id } });
-
-    if (!news) {
-      throw new NotFoundException('News not found');
+    if (!news.length) {
+      throw new NotFoundException('No news found');
     }
 
     return news;
   }
 
-  async update(id: number, updateNewsDto: UpdateNewsDto) {
+  async findOne(id: number, token: string) {
     const news = await this.prisma.news.findUnique({ where: { id } });
 
     if (!news) {
       throw new NotFoundException('News not found');
     }
+
+    if (token['roleId'] === 1) {
+      return news;
+    } else if (token['roleId'] === 2 || token['roleId'] === 3) {
+      if (!token['roleId']) {
+        throw new ForbiddenException('You are not assigned to any company');
+      }
+
+      if (news.companyId !== token['roleId']) {
+        throw new ForbiddenException('Access denied to this news');
+      }
+
+      return news;
+    } else {
+      throw new ForbiddenException('Access denied');
+    }
+  }
+
+  async update(id: number, updateNewsDto: UpdateNewsDto, token: string) {
+    await this.findOne(id, token);
 
     return this.prisma.news.update({ where: { id }, data: updateNewsDto });
   }
 
-  async remove(id: number) {
-    const news = await this.prisma.news.findUnique({ where: { id } });
-
-    if (!news) {
-      throw new NotFoundException('News not found');
-    }
-
+  async remove(id: number, token: string) {
+    await this.findOne(id, token);
     await this.prisma.news.delete({ where: { id } });
 
     return { message: 'News deleted successfully' };
