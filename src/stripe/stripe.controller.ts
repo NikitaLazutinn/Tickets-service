d49c048691eeba699fb1ee3d5f3e6e552e04afff
Oleksandr Dushnyi <sdusnij@gmail.com>
2025-01-08 11:07:12 +0200

    fix: update stripe

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,15 +1,54 @@
-import { Controller, Post, Body, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Query,
+  BadRequestException,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { StripeService } from './stripe.service';
+import Stripe from 'stripe';
+import { AuthUserGuard } from 'src/guards';
 
 @Controller('stripe')
 export class StripeController {
   constructor(private readonly stripeService: StripeService) {}
 
+  @UseGuards(AuthUserGuard)
   @Post('create-session/:ticketId')
   async createPaymentSession(
     @Param('ticketId') ticketId: string,
     @Query('userId') userId: number,
+    @Req() req,
   ) {
-    return this.stripeService.createPaymentSession(+ticketId, userId);
+    const token = req.user;
+    return await this.stripeService.createPaymentSession(
+      +ticketId,
+      userId,
+      token,
+    );
+  }
+
+  @UseGuards(AuthUserGuard)
+  @Post('webhook')
+  async handleStripeWebhook(@Body() payload: any, @Req() req) {
+    const token = req.user;
+    const sig = payload['stripe-signature'];
+    const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+    const event: Stripe.Event =
+      this.stripeService.stripe.webhooks.constructEvent(
+        payload,
+        sig,
+        endpointSecret,
+      );
+
+    if (!event) {
+      throw new BadRequestException('Error verifying webhook signature');
+    }
+
+    return await this.stripeService.handleStripeWebhook(event, token);
   }
 }
--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,21 +1,23 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'prisma/prisma.service';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import Stripe from 'stripe';
+import { TicketService } from 'src/ticket/ticket.service';
 
 @Injectable()
 export class StripeService {
-  private stripe: Stripe;
+  stripe: Stripe;
 
-  constructor(private prisma: PrismaService) {
+  constructor(private ticketService: TicketService) {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
       apiVersion: null,
     });
   }
 
-  async createPaymentSession(ticketId: number, userId: number) {
-    const ticket = await this.prisma.ticket.findUnique({
-      where: { id: ticketId },
-    });
+  async createPaymentSession(ticketId: number, userId: number, token: string) {
+    const ticket = await this.ticketService.findOne(ticketId, token);
 
     if (!ticket) {
       throw new NotFoundException('Ticket not found');
@@ -33,7 +35,7 @@ export class StripeService {
           price_data: {
             currency: 'usd',
             product_data: {
-              name: `Ticket for Event ${ticket.eventId}`,
+              name: `Ticket for event ${ticket.eventId}`,
               description: `Seat: ${ticket.seatNumber}`,
             },
             unit_amount: ticket.price * 100,
@@ -41,13 +43,48 @@ export class StripeService {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.LOCALHOST_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.LOCALHOST_URL}/success?session_id={CHECKOUT_SESSION_ID}`, // Fixed string interpolation
       cancel_url: `${process.env.LOCALHOST_URL}/cancel`,
       metadata: {
         userId: userId.toString(),
+        ticketId: ticketId.toString(),
       },
     });
 
     return session;
+  }
+
+  async handleStripeWebhook(event: Stripe.Event, token: string) {
+    switch (event.type) {
+      case 'checkout.session.completed': {
+        const session = event.data.object as Stripe.Checkout.Session;
+        const ticketId = parseInt(session.metadata.ticketId);
+        const userId = parseInt(session.metadata.userId);
+
+        if (session.payment_status !== 'paid') {
+          throw new InternalServerErrorException('Payment was not successful');
+        }
+
+        try {
+          await this.ticketService.generateTicketPdfAndUploadToDropbox(
+            userId.toString(),
+            ticketId,
+          );
+        } catch (error) {
+          throw new InternalServerErrorException(
+            'Error during post-payment processing',
+          );
+        }
+        break;
+      }
+      case 'checkout.session.async_payment_failed': {
+        const failedSession = event.data.object as Stripe.Checkout.Session;
+        const failedTicketId = parseInt(failedSession.metadata.ticketId);
+        await this.ticketService.remove(failedTicketId, token);
+        break;
+      }
+      default:
+        console.log(`Unknown event: ${event.type}`);
+    }
   }
 }
